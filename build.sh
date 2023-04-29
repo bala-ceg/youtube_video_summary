1fa96605c6407ac92e1dd94e3454d9457f559653
Balaji Seetharaman <balaji_ceg@outlook.com>
2023-04-29 16:21:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install -r requirements.txt
 
-echo "Installing the latest version of poetry..."
-pip install --upgrade pip
-pip install poetry==1.2.0
 
-rm poetry.lock
-
-poetry lock
-
-python -m poetry install
+python manage.py collectstatic --no-input
+python manage.py migrate
