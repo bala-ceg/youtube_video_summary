d8bc7963dcbef6b3e49bd09bffcec99214769370
Balaji Seetharaman <balaji_ceg@outlook.com>
2023-04-29 16:02:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,3 @@ rm poetry.lock
 poetry lock
 
 python -m poetry install
-
-/opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
-/opt/render/project/src/.venv/bin/python manage.py migrate
