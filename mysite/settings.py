89a11d8a96f564ef9ecba3bc5f9a26a1ec86d1c1
Balaji Seetharaman <balaji_ceg@outlook.com>
2023-04-30 09:01:43 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = ['https://youtubevideosummary-production.up.railway.app']
+
+CORS_ORIGIN_WHITELIST = ['https://youtubevideosummary-production.up.railway.app']
 
 # Application definition
 
